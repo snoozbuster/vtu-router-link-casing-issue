f722a3d592048823cb8795d87a539fb370e4692d
Alex Van Liew <alexv@productiv.com>
2021-02-25 10:55:52 -0800

    oops, no src/\!

---
+++ src/components/MyLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <RouterLink class="link" :to="{ name: name }">
+    <slot>
+      A test link
+    </slot>
+  </RouterLink>
+</template>
+
+<script>
+export default {
+  name: "MyLink",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
